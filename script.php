40aef4741ae0ac840fdb7cfc14df5328c1467041
raimundo.marques <raimundo.marques.ff@gmail.com>
2023-09-05 14:59:53 -0400

    Atualização do inserto da tabela contrato

--- script.php
+++ script.php
@@ -11,7 +11,6 @@ $cria_tb_banco = "
 	nome varchar(100)
     );
 ";
-
 $insert_tb_banco = "
     insert into tb_banco (id_banco, nome)
     values (1, 'Banco do Brasil'),
@@ -19,7 +18,7 @@ $insert_tb_banco = "
     (3, 'Itaú'),
     (4, 'Caixa Econômica Federal'),
     (5, 'Santander'),
-    (6, 'Banco da Amazônia')
+    (6, 'Banco da Amazônia');
 ";
 
 
@@ -34,7 +33,6 @@ $cria_tb_convenio = "
 	constraint fk_banco foreign key (id_banco) references tb_banco (id_banco)
     );
 ";
-
 $insert_tb_convenio = "
 insert into tb_convenio (id_convenio, convenio, verba, id_banco)
 values (1, 'Fábrica de Eventos', 3000000, 2),
@@ -42,7 +40,7 @@ values (1, 'Fábrica de Eventos', 3000000, 2),
     (3, 'Santa Júlia', 250000, 4),
     (4, 'HAPIVIDA', 110550, 5),
     (5, 'Bradesco Saúde', 200780, 1),
-    (6, 'Plano dentário', 175890, 3)
+    (6, 'Plano dentário', 175890, 3);
 ";
 
 
@@ -58,7 +56,6 @@ $cria_tb_convenio_servico = "
 	constraint fk_conven_serv foreign key (id_convenio) references tb_convenio (id_convenio)
     );
 ";
-
 $insert_tb_convenio_servico = "
 insert into tb_convenio_servico (id_convenio_servico, id_convenio, servico)
 values (1, 2, 'Pintura'),
@@ -70,7 +67,7 @@ values (1, 2, 'Pintura'),
     (7, 4, 'Empleita'),
     (8, 5, 'Empleita'),
     (9, 5, 'Construção de uma Sala de Recepção'),
-    (10, 4, 'Serviços de T.I')
+    (10, 4, 'Serviços de T.I');
 ";
 
 
@@ -88,23 +85,22 @@ $cria_tb_contrato = "
 	constraint fk_conServ foreign key (id_convenio_servico) references tb_convenio_servico (id_convenio_servico)
     );
 ";
-
 $insert_tb_contrato = "
-insert into tb_contrato (id_contrato, prazo, valor, data_inclusao, id_convenio_servico)
-values (1,'2025-01-01', 150000, now(), 2), 
-    (2, '2030-01-01', 250.000, now(), 5), 
-    (3, '2024-06-01', 180.750, now(), 3), 
-    (4, '2026-05-15', 750.000, now(), 6), 
-    (5, '2028-10-07', 550.250, now(), 4),
-    (6, '2029-06-15', 350.250, now(), 4),
-    (7, '2027-05-20', 200.250, now(), 2),
-    (8, '2026-10-18', 200.000, now(), 2),
-    (9, '2028-11-05', 150.150, now(), 3),
-    (10, '2025-03-07', 800.250, now(), 5),
-    (11, '2030-01-10', 830.820, now(), 5),
-    (12, '2031-05-20', 105.860, now(), 1),
-    (13, '2033-01-23', 75.000, now(), 1),
-    (14, '2035-11-04', 25.830, now(), 1)
+INSERT INTO public.tb_contrato (id_contrato,prazo,valor,data_inclusao,id_convenio_servico)
+VALUES (2,'2030-01-01',250.0,'2022-08-03 10:10:09.943',5),
+	 (3,'2024-06-01',180.75,'2018-01-03 10:10:09.943',3),
+	 (4,'2026-05-15',750.0,'2017-10-03 10:10:09.943',6),
+	 (5,'2028-10-07',550.25,'2015-03-10 10:10:09.943',4),
+	 (6,'2029-06-15',350.25,'2020-07-05 10:10:09.943',4),
+	 (7,'2027-05-20',200.25,'2013-10-15 10:10:09.943',2),
+	 (8,'2026-10-18',200.0,'2013-12-25 10:10:09.943',2),
+	 (9,'2028-11-05',150.15,'2011-03-10 10:10:09.943',3),
+	 (10,'2025-03-07',800.25,'2011-05-10 10:10:09.943',5),
+	 (11,'2030-01-10',830.82,'2005-05-25 10:10:09.943',5),
+	 (12,'2031-05-20',105.86,'2011-12-30 10:10:09.943',1),
+	 (13,'2033-01-23',75.0,'2011-12-25 10:10:09.943',1),
+	 (14,'2035-11-04',25.83,'2011-10-07 10:10:09.943',1),
+	 (1,'2025-01-01',150000.0,'2015-07-10 10:10:09.943',2);
 ";
 
 
